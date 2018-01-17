c4c984acc898c61118e46ce4c8d02750b22bc96c
Hannu <hannu.tapanila@gmail.com>
2018-01-17 20:59:40 +0200

    Poll data providers

---
+++ src/providers/index.ts
@@ -0,0 +1,40 @@
+import * as moment from 'moment';
+import winstonInstance from '../config/winston';
+import { OuluProvider } from '../providers/oulu';
+
+export interface DataProvider {
+  name: string;
+  updateInterval: number;
+  getTripUpdates: () => Promise<number>;
+}
+
+export interface DataProviderStatus {
+  updated: string;
+  lastTripUpdateCount: number;
+}
+
+const dataProviders: DataProvider[] = [new OuluProvider()];
+
+export const statusDataMap = new Map<string, DataProviderStatus>();
+
+export async function startDataProviders() {
+  for (const dataProvider of dataProviders) {
+    console.info(`Starting data provider ${dataProvider.name}`);
+
+    const getData = async () => {
+      try {
+        const tripUpdateCount = await dataProvider.getTripUpdates();
+        statusDataMap.set(dataProvider.name, {
+          updated: moment().format(),
+          lastTripUpdateCount: tripUpdateCount,
+        });
+      } catch (error) {
+        winstonInstance.error(`Data provider ${dataProvider.name} error`, error.message);
+      } finally {
+        setTimeout(getData, dataProvider.updateInterval);
+      }
+    };
+
+    getData();
+  }
+}
