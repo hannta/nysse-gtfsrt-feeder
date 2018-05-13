98c977304b072a2a75dbc1072a52eb272ed910d4
Hannu <hannu.tapanila@gmail.com>
2018-05-13 23:42:41 +0300

    Switch from outdated mariasql database lib to mysql2

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,19 +2,19 @@ import * as Knex from 'knex';
 import config from '../config/config';
 
 const knexConfig: Knex.Config = {
-  client: 'mariadb',
+  client: 'mysql2',
   connection: {
     host: config.database.host,
     user: config.database.user,
     password: config.database.password,
-    db: config.database.database,
+    database: config.database.database,
     charset: 'utf8',
-    debug: config.database.debug,
   },
   pool: {
     min: config.database.poolConnectionMin,
     max: config.database.poolConnectionMax,
   },
+  debug: config.database.debug,
 };
 
 export const knex = Knex(knexConfig);
