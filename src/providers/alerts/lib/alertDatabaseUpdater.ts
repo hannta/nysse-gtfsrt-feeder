3f84fb3971958a3e194386d9d5c9f84b1a1b6060
Hannu <hannu.tapanila@gmail.com>
2020-02-22 23:48:31 +0200

    Add alert database updater

---
+++ src/providers/alerts/lib/alertDatabaseUpdater.ts
@@ -0,0 +1,35 @@
+import { insertOrUpdate, emptyTable } from '../../../lib/databaseUtils';
+import { AlertDB, InformedEntityDB, TranslatedTextDB } from '../../../types';
+
+const ALERTS_TABLE = 'alerts';
+const ALERT_INFORMED_ENTITIES_TABLE = 'alert_informed_entities';
+const ALERT_HEADER_TEXTS_TABLE = 'alert_header_texts';
+const ALERT_DESCRIPTION_TEXTS_TABLE = 'alert_description_texts';
+const ALERT_URLS_TABLE = 'alert_urls';
+
+export async function updateAlertsDatabase(
+  regionKey: string,
+  alerts: AlertDB[],
+  alertInformedEntities: InformedEntityDB[],
+  alertHeaderTexts: TranslatedTextDB[],
+  alertDescriptionTexts: TranslatedTextDB[],
+  alertUrls: TranslatedTextDB[],
+) {
+  // TODO do this in one transaction
+
+  const alertsTable = `${regionKey}_${ALERTS_TABLE}`;
+  await emptyTable(alertsTable);
+  await insertOrUpdate(alertsTable, alerts);
+
+  const alertInformedEntitiesTable = `${regionKey}_${ALERT_INFORMED_ENTITIES_TABLE}`;
+  await insertOrUpdate(alertInformedEntitiesTable, alertInformedEntities);
+
+  const alertHeaderTextsTable = `${regionKey}_${ALERT_HEADER_TEXTS_TABLE}`;
+  await insertOrUpdate(alertHeaderTextsTable, alertHeaderTexts);
+
+  const alertDescriptionTextsTable = `${regionKey}_${ALERT_DESCRIPTION_TEXTS_TABLE}`;
+  await insertOrUpdate(alertDescriptionTextsTable, alertDescriptionTexts);
+
+  const alertUrlsTable = `${regionKey}_${ALERT_URLS_TABLE}`;
+  await insertOrUpdate(alertUrlsTable, alertUrls);
+}
