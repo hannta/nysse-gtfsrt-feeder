ce609b5f4b86eb024d56cbd0be742c56b2ce1716
Hannu <hannu.tapanila@gmail.com>
2020-02-22 23:50:17 +0200

    Add gtfs rt alert provider

---
+++ src/providers/alerts/GtfsRtAlertProvider.ts
@@ -0,0 +1,45 @@
+import Axios, { AxiosRequestConfig } from 'axios';
+import { GtfsRTAlertFeedProcessor } from './lib/GtfsRTAlertFeedProcessor';
+import config from '../../config/config';
+import { AlertDataProvider } from '.';
+
+export class GtfsRtAlertProvider implements AlertDataProvider {
+  public readonly regionKey: string;
+
+  public readonly updateInterval: number;
+
+  private readonly gtfsRTFeedUrl: string;
+
+  private readonly requestHeaders?: { [k: string]: string };
+
+  private readonly gtfsRTAlertFeedProcessor: GtfsRTAlertFeedProcessor;
+
+  constructor(
+    regionKey: string,
+    gtfsRTFeedUrl: string,
+    updateInterval: number,
+    requestHeaders?: { [k: string]: string },
+  ) {
+    this.regionKey = regionKey;
+    this.gtfsRTFeedUrl = gtfsRTFeedUrl;
+    this.updateInterval = updateInterval;
+    this.requestHeaders = requestHeaders;
+    this.gtfsRTAlertFeedProcessor = new GtfsRTAlertFeedProcessor(regionKey);
+  }
+
+  public async getAlerts() {
+    const requestConfig: AxiosRequestConfig = {
+      method: 'GET',
+      url: this.gtfsRTFeedUrl,
+      responseType: 'arraybuffer',
+      headers: {
+        'User-Agent': config.serverUserAgent,
+        ...this.requestHeaders,
+      },
+      timeout: 5000,
+    };
+
+    const resp = await Axios.request(requestConfig);
+    return this.gtfsRTAlertFeedProcessor.storeAlertsUpdateFeed(resp.data);
+  }
+}
