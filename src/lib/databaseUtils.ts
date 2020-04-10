ae6d042c6e238fb5aaa7c2cb28f8d82b5e0542b5
Hannu <hannu.tapanila@gmail.com>
2020-04-10 15:59:28 +0300

    Dont allways keep old records

--- src/lib/databaseUtils.ts
+++ src/lib/databaseUtils.ts
@@ -1,10 +1,11 @@
 import { knex } from '../config/database';
+import Knex from 'knex';
 
 /**
  * Insert or update data
  * @param tableName
  * @param data
- */
+ *
 export async function insertOrUpdate(tableName: string, data: object[]) {
   if (data.length < 1) {
     return;
@@ -12,12 +13,26 @@ export async function insertOrUpdate(tableName: string, data: object[]) {
 
   const firstData = data[0] ? data[0] : data;
   return knex.raw(
-    knex(tableName)
-      .insert(data)
-      .toQuery() +
+    knex(tableName).insert(data).toQuery() +
       ' ON DUPLICATE KEY UPDATE ' +
       Object.getOwnPropertyNames(firstData)
-        .map(field => `${field}=VALUES(${field})`)
+        .map((field) => `${field}=VALUES(${field})`)
+        .join(', '),
+  );
+}
+*/
+
+export async function insertOrUpdate(
+  knexTransaction: Knex.Transaction,
+  tableName: string,
+  data: object[],
+) {
+  const firstData = data[0] ? data[0] : data;
+  return knexTransaction.raw(
+    knexTransaction(tableName).insert(data).toQuery() +
+      ' ON DUPLICATE KEY UPDATE ' +
+      Object.getOwnPropertyNames(firstData)
+        .map((field) => `${field}=VALUES(${field})`)
         .join(', '),
   );
 }
--- src/providers/tripUpdates/lib/tripUpdateDatabaseUpdater.ts
+++ src/providers/tripUpdates/lib/tripUpdateDatabaseUpdater.ts
@@ -1,6 +1,7 @@
 import moment from 'moment';
 import { knex } from '../../../config/database';
 import { insertOrUpdate } from '../../../lib/databaseUtils';
+import Knex from 'knex';
 
 const TRIP_UPDATES_TABLE = 'trip_updates';
 const TRIP_UPDATE_STOP_TIME_UPDATES_TABLE = 'trip_update_stop_time_updates';
@@ -40,6 +41,7 @@ export interface StopTimeUpdateDB {
  * @param tripUpdateStopTimeUpdates
  */
 export async function updateDatabase(
+  isFullDataset: boolean,
   regionName: string,
   tripUpdates: TripUpdateDB[],
   tripUpdateStopTimeUpdates: StopTimeUpdateDB[],
@@ -48,27 +50,32 @@ export async function updateDatabase(
   const tripUpdatesTable = `${regionName}_${TRIP_UPDATES_TABLE}`;
   const tripUpdateStopTimeUpdatesTable = `${regionName}_${TRIP_UPDATE_STOP_TIME_UPDATES_TABLE}`;
 
-  if (tripUpdates && tripUpdates.length > 0) {
-    await insertOrUpdate(tripUpdatesTable, tripUpdates);
-  }
+  return knex.transaction(async (trx) => {
+    if (isFullDataset) {
+      trx(tripUpdatesTable).del();
+    } else {
+      await deleteOldData(trx, tripUpdatesTable, keepOldRecords);
+    }
 
-  if (tripUpdateStopTimeUpdates && tripUpdateStopTimeUpdates.length > 0) {
-    await insertOrUpdate(tripUpdateStopTimeUpdatesTable, tripUpdateStopTimeUpdates);
-  }
+    if (tripUpdates && tripUpdates.length > 0) {
+      await insertOrUpdate(trx, tripUpdatesTable, tripUpdates);
+    }
 
-  await deleteOldData(tripUpdatesTable, keepOldRecords);
+    if (tripUpdateStopTimeUpdates && tripUpdateStopTimeUpdates.length > 0) {
+      await insertOrUpdate(trx, tripUpdateStopTimeUpdatesTable, tripUpdateStopTimeUpdates);
+    }
+  });
 }
 
 /**
  * Delete old trip updates
  * @param tripUpdatesTable
  */
-async function deleteOldData(tripUpdatesTable: string, keepOldRecords = KEEP_OLD_RECORDS_DEFAULT) {
-  const olderThan = moment()
-    .subtract(keepOldRecords, 'seconds')
-    .format('YYYY-MM-DD HH:mm:ss');
-
-  return knex(tripUpdatesTable)
-    .where('recorded', '<', olderThan)
-    .delete();
+async function deleteOldData(
+  knexTransaction: Knex.Transaction,
+  tripUpdatesTable: string,
+  keepOldRecords = KEEP_OLD_RECORDS_DEFAULT,
+) {
+  const olderThan = moment().subtract(keepOldRecords, 'seconds').format('YYYY-MM-DD HH:mm:ss');
+  return knexTransaction(tripUpdatesTable).where('recorded', '<', olderThan).delete();
 }
